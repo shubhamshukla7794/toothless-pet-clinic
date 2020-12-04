e5c2304ec8ed20538ec5295c7bc393d5e92a0747
shubhamshukla7794 <shubhamshukla7794@gmail.com>
2020-12-04 10:41:16 +0530

    Initial Project Add

---
+++ src/main/java/com/shubham/toothlesspetclinic/ToothlessPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.shubham.toothlesspetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToothlessPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToothlessPetClinicApplication.class, args);
+    }
+
+}
