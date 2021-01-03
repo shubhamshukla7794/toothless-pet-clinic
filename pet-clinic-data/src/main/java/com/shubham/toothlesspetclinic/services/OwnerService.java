9bf675b32be225a915ea6fa36f239763562202a0
shubhamshukla7794 <shubhamshukla7794@gmail.com>
2021-01-03 19:10:19 +0530

    Refactoring to common CRUD Interface. Closes #11

--- pet-clinic-data/src/main/java/com/shubham/toothlesspetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/shubham/toothlesspetclinic/services/OwnerService.java
@@ -2,11 +2,7 @@ package com.shubham.toothlesspetclinic.services;
 
 import com.shubham.toothlesspetclinic.model.Owner;
 
-import java.util.Set;
 
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String lastName);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/com/shubham/toothlesspetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/shubham/toothlesspetclinic/services/PetService.java
@@ -2,10 +2,6 @@ package com.shubham.toothlesspetclinic.services;
 
 import com.shubham.toothlesspetclinic.model.Pet;
 
-import java.util.Set;
 
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long>{
 }
