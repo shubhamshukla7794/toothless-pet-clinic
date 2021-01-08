22da8dfa9f1f135b5f1d874cffb9cbf2b1b0378b
shubhamshukla7794 <shubhamshukla7794@gmail.com>
2021-01-08 18:33:32 +0530

    Added Specialties Map Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/com/shubham/toothlesspetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package com.shubham.toothlesspetclinic.services;
+
+import com.shubham.toothlesspetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
