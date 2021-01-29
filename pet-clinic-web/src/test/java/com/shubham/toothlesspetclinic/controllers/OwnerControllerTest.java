13f0a040e10b59dba1c0f1b55d9df790834c69aa
shubhamshukla7794 <shubhamshukla7794@gmail.com>
2021-01-29 21:06:29 +0530

    Displayed Owner Record. Closes #53

--- pet-clinic-web/src/test/java/com/shubham/toothlesspetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/com/shubham/toothlesspetclinic/controllers/OwnerControllerTest.java
@@ -15,7 +15,8 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -67,5 +68,14 @@ class OwnerControllerTest {
                 .andExpect(view().name("notimplemented"));
 
         verifyNoInteractions(ownerService);
+    }
+
+    @Test
+    void displayOwners() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1L).build());
+        mockMvc.perform(MockMvcRequestBuilders.get("/owners/123"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownerDetails"))
+                .andExpect(model().attribute("owner", hasProperty("id", is(1L))));
     }
 }
