f58006c2201c987b0de6739bc30cb745c995268d
shubhamshukla7794 <shubhamshukla7794@gmail.com>
2021-02-27 17:37:52 +0530

    Implemented H2 DB Script for default values. Closes #71

--- pet-clinic-web/src/main/java/com/shubham/toothlesspetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/shubham/toothlesspetclinic/bootstrap/DataLoader.java
@@ -1,116 +1,116 @@
-package com.shubham.toothlesspetclinic.bootstrap;
-
-import com.shubham.toothlesspetclinic.model.*;
-import com.shubham.toothlesspetclinic.services.*;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
-
-import java.time.LocalDate;
-
-@Component
-public class DataLoader implements CommandLineRunner {
-
-    private final OwnerService ownerService;
-    private final VetService vetService;
-    private final PetTypeService petTypeService;
-    private final SpecialtyService specialtyService;
-    private final VisitService visitService;
-
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService,
-                      SpecialtyService specialtyService, VisitService visitService) {
-        this.ownerService = ownerService;
-        this.vetService = vetService;
-        this.petTypeService = petTypeService;
-        this.specialtyService = specialtyService;
-        this.visitService = visitService;
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-
-        int count = petTypeService.findAll().size();
-
-        if (count == 0) {
-            loadData();
-        }
-    }
-
-    private void loadData() {
-        PetType dog = new PetType();
-        dog.setName("Dog");
-        PetType savedDogPetType = petTypeService.save(dog);
-
-        PetType owl = new PetType();
-        owl.setName("Owl");
-        PetType savedOwlPetType = petTypeService.save(owl);
-
-        Owner owner1 = new Owner();
-        owner1.setFirstName("Dean");
-        owner1.setLastName("Winchester");
-        owner1.setAddress("123 Super St.");
-        owner1.setCity("Lebanon");
-        owner1.setTelephone("7855550128");
-
-        Pet deansDog = new Pet();
-        deansDog.setPetType(savedDogPetType);
-        deansDog.setOwner(owner1);
-        deansDog.setBirthDate(LocalDate.now());
-        deansDog.setName("Miracle");
-        owner1.getPets().add(deansDog);
-
-        ownerService.save(owner1);
-
-        Owner owner2 = new Owner();
-        owner2.setFirstName("Harry");
-        owner2.setLastName("Potter");
-        owner2.setAddress("4 Private Drive");
-        owner2.setCity("London");
-        owner2.setTelephone("6054756961");
-
-        Pet harrysOwl = new Pet();
-        harrysOwl.setPetType(savedOwlPetType);
-        harrysOwl.setOwner(owner2);
-        harrysOwl.setBirthDate(LocalDate.now());
-        harrysOwl.setName("Hedwig");
-        owner2.getPets().add(harrysOwl);
-
-        ownerService.save(owner2);
-
-        Visit owlVisit = new Visit();
-        owlVisit.setPet(harrysOwl);
-        owlVisit.setDate(LocalDate.now());
-        owlVisit.setDescription("Sneezy Owl");
-
-        visitService.save(owlVisit);
-
-        System.out.println("Loaded Owners......");
-
-        Specialty radiology = new Specialty();
-        radiology.setDescription("Radiology");
-        Specialty savedRadiology = specialtyService.save(radiology);
-
-        Specialty surgery = new Specialty();
-        surgery.setDescription("Surgery");
-        Specialty savedSurgery = specialtyService.save(surgery);
-
-        Specialty dentistry = new Specialty();
-        dentistry.setDescription("Dentistry");
-        Specialty savedDentistry = specialtyService.save(dentistry);
-
-        Vet vet1 = new Vet();
-        vet1.setFirstName("Bobby");
-        vet1.setLastName("Singer");
-        vet1.getSpecialties().add(savedRadiology);
-
-        vetService.save(vet1);
-
-        Vet vet2 = new Vet();
-        vet2.setFirstName("Rubeus");
-        vet2.setLastName("Hagrid");
-        vet2.getSpecialties().add(savedSurgery);
-
-        vetService.save(vet2);
-
-        System.out.println("Loaded Vets......");
-    }
-}
+//package com.shubham.toothlesspetclinic.bootstrap;
+//
+//import com.shubham.toothlesspetclinic.model.*;
+//import com.shubham.toothlesspetclinic.services.*;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.stereotype.Component;
+//
+//import java.time.LocalDate;
+//
+//@Component
+//public class DataLoader implements CommandLineRunner {
+//
+//    private final OwnerService ownerService;
+//    private final VetService vetService;
+//    private final PetTypeService petTypeService;
+//    private final SpecialtyService specialtyService;
+//    private final VisitService visitService;
+//
+//    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService,
+//                      SpecialtyService specialtyService, VisitService visitService) {
+//        this.ownerService = ownerService;
+//        this.vetService = vetService;
+//        this.petTypeService = petTypeService;
+//        this.specialtyService = specialtyService;
+//        this.visitService = visitService;
+//    }
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//
+//        int count = petTypeService.findAll().size();
+//
+//        if (count == 0) {
+//            loadData();
+//        }
+//    }
+//
+//    private void loadData() {
+//        PetType dog = new PetType();
+//        dog.setName("Dog");
+//        PetType savedDogPetType = petTypeService.save(dog);
+//
+//        PetType owl = new PetType();
+//        owl.setName("Owl");
+//        PetType savedOwlPetType = petTypeService.save(owl);
+//
+//        Owner owner1 = new Owner();
+//        owner1.setFirstName("Dean");
+//        owner1.setLastName("Winchester");
+//        owner1.setAddress("123 Super St.");
+//        owner1.setCity("Lebanon");
+//        owner1.setTelephone("7855550128");
+//
+//        Pet deansDog = new Pet();
+//        deansDog.setPetType(savedDogPetType);
+//        deansDog.setOwner(owner1);
+//        deansDog.setBirthDate(LocalDate.now());
+//        deansDog.setName("Miracle");
+//        owner1.getPets().add(deansDog);
+//
+//        ownerService.save(owner1);
+//
+//        Owner owner2 = new Owner();
+//        owner2.setFirstName("Harry");
+//        owner2.setLastName("Potter");
+//        owner2.setAddress("4 Private Drive");
+//        owner2.setCity("London");
+//        owner2.setTelephone("6054756961");
+//
+//        Pet harrysOwl = new Pet();
+//        harrysOwl.setPetType(savedOwlPetType);
+//        harrysOwl.setOwner(owner2);
+//        harrysOwl.setBirthDate(LocalDate.now());
+//        harrysOwl.setName("Hedwig");
+//        owner2.getPets().add(harrysOwl);
+//
+//        ownerService.save(owner2);
+//
+//        Visit owlVisit = new Visit();
+//        owlVisit.setPet(harrysOwl);
+//        owlVisit.setDate(LocalDate.now());
+//        owlVisit.setDescription("Sneezy Owl");
+//
+//        visitService.save(owlVisit);
+//
+//        System.out.println("Loaded Owners......");
+//
+//        Specialty radiology = new Specialty();
+//        radiology.setDescription("Radiology");
+//        Specialty savedRadiology = specialtyService.save(radiology);
+//
+//        Specialty surgery = new Specialty();
+//        surgery.setDescription("Surgery");
+//        Specialty savedSurgery = specialtyService.save(surgery);
+//
+//        Specialty dentistry = new Specialty();
+//        dentistry.setDescription("Dentistry");
+//        Specialty savedDentistry = specialtyService.save(dentistry);
+//
+//        Vet vet1 = new Vet();
+//        vet1.setFirstName("Bobby");
+//        vet1.setLastName("Singer");
+//        vet1.getSpecialties().add(savedRadiology);
+//
+//        vetService.save(vet1);
+//
+//        Vet vet2 = new Vet();
+//        vet2.setFirstName("Rubeus");
+//        vet2.setLastName("Hagrid");
+//        vet2.getSpecialties().add(savedSurgery);
+//
+//        vetService.save(vet2);
+//
+//        System.out.println("Loaded Vets......");
+//    }
+//}
---
+++ pet-clinic-web/src/main/resources/data.sql
@@ -0,0 +1,53 @@
+INSERT INTO vets VALUES (1, 'Bobby', 'Singer');
+INSERT INTO vets VALUES (2, 'Rubeus', 'Hagrid');
+INSERT INTO vets VALUES (3, 'Garth', 'Fitzgerald IV');
+INSERT INTO vets VALUES (4, 'Newt', 'Scamander');
+INSERT INTO vets VALUES (5, 'Hiccup', 'Haddock III');
+INSERT INTO vets VALUES (6, 'Remus', 'Lupin');
+
+INSERT INTO specialties VALUES (1, 'Radiology');
+INSERT INTO specialties VALUES (2, 'Surgery');
+INSERT INTO specialties VALUES (3, 'Dentistry');
+
+INSERT INTO vet_specialties VALUES (1, 1);
+INSERT INTO vet_specialties VALUES (2, 2);
+INSERT INTO vet_specialties VALUES (3, 3);
+INSERT INTO vet_specialties VALUES (4, 2);
+INSERT INTO vet_specialties VALUES (5, 1);
+
+INSERT INTO types VALUES (1, 'Dog');
+INSERT INTO types VALUES (2, 'Owl');
+INSERT INTO types VALUES (3, 'Hippogriff');
+INSERT INTO types VALUES (4, 'Snake');
+INSERT INTO types VALUES (5, 'Phoenix');
+INSERT INTO types VALUES (6, 'Cat');
+
+INSERT INTO owners VALUES (1, 'Dean', 'Winchester', '123 Super St.', 'Lebanon', '7855550128');
+INSERT INTO owners VALUES (2, 'Harry', 'Potter', '4 Private Drive', 'London', '6054756961');
+INSERT INTO owners VALUES (3, 'Sirius', 'Black', '12 Grimmauld Place', 'London', '6085558763');
+INSERT INTO owners VALUES (4, 'Sam', 'Winchester', '123 Super St.', 'Lebanon', '6085553198');
+INSERT INTO owners VALUES (5, 'Ronald', 'Weasley', 'Ottery St Catchpole', 'Devon', '6085552765');
+INSERT INTO owners VALUES (6, 'Albus', 'Dumbledore', '105 Godric&#39;s Hollow', 'Cornwall', '6085552654');
+INSERT INTO owners VALUES (7, 'Tom', 'Riddle', '1450 Little Hangleton', 'Yorkshire', '6085555387');
+INSERT INTO owners VALUES (8, 'Shaggy', 'Rogers', 'Coolsville', 'Ohio', '6085557683');
+INSERT INTO owners VALUES (9, 'John', 'Wick', '2749 Horseshoe Road', 'Mill Neck, N.Y.', '6085559435');
+INSERT INTO owners VALUES (10, 'Hermione', 'Granger', '2335 Independence St.', 'Hampstead', '6085555487');
+
+INSERT INTO pets VALUES (1, '2010-09-07', 'Hedwig',  2, 2);
+INSERT INTO pets VALUES (2, '2012-08-06', 'Miracle',  1, 1);
+INSERT INTO pets VALUES (3, '2011-04-17', 'BuckBeak', 3, 3);
+INSERT INTO pets VALUES (4, '2010-03-07', 'Bones', 4, 1);
+INSERT INTO pets VALUES (5, '2010-11-30', 'Errol', 5, 2);
+INSERT INTO pets VALUES (6, '2010-01-20', 'Fawkes', 6, 5);
+INSERT INTO pets VALUES (7, '2012-09-04', 'Nagini', 7, 4);
+INSERT INTO pets VALUES (8, '2012-09-04', 'Scooby-Doo', 8, 1);
+INSERT INTO pets VALUES (9, '2011-08-06', 'Brutus', 9, 1);
+INSERT INTO pets VALUES (10, '2007-02-24', 'Crookshanks', 10, 6);
+INSERT INTO pets VALUES (11, '2010-03-09', 'Scrappy-Doo', 8, 1);
+INSERT INTO pets VALUES (12, '2010-06-24', 'Riot', 4, 1);
+INSERT INTO pets VALUES (13, '2012-06-08', 'Pigwidgeon', 6, 2);
+
+INSERT INTO visits VALUES (1, '2020-01-01', 'Rabies shot', 4);
+INSERT INTO visits VALUES (2, '2021-01-02', 'Rabies shot', 2);
+INSERT INTO visits VALUES (3, '2021-01-03', 'Sneezy Owl', 1);
+INSERT INTO visits VALUES (4, '2020-01-04', 'Rebirth', 6);
